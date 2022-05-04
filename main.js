f9c6de25e51178201a91492364cc7af463c129a2
celesrodriguez <celeste.rodriguez97@hotmail.com>
2022-05-04 02:09:40 -0300

    read more now goes to technologies

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ window.onload = function(){
         if (!document.getElementById('menu-id').contains(e.target) && (!document.getElementById('navbar-id').contains(e.target))){
             console.log("Clicked outside menu");
             document.getElementById('click').checked=false; //the same code you've used to hide the menu
-      } 
+        } 
+
+        // if (document.getElementById('technologies-section').contains(e.target)){
+        //     console.log("Clicked technologies-section");
+        //     const scrollingElement = (document.scrollingElement || document.body);
+        //     scrollingElement.scrollTop = scrollingElement.scrollHeight;
+        // } 
     })
-    
+
+    setTimeout(function() {
+        window.scrollTo(0,document.body.scrollHeight);
+    }, 1500);
 }
